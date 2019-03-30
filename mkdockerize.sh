1967744315ddac89890e952ebc3ad317e41f22b5
lajmahal <laj.lal@outlook.com>
2019-03-30 13:39:00 +0800

    The script assumes that a mkdocs project has been mounted at a pre-determined location. This is based on the assumption that "produce" or "serve" are the only arguments to the script itself, and therefore the path to the mkdocs project itself must have been mounted to a predetermined path via an argument to "docker run" itself. The script will exit if the path does not exist, with an error message suggesting that the user didn't mount the project to the right path when running the docker image.

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+mkdocsProjectPath="/data/mkdocs-project"
 if [[ $# -ne 1 ]]; then
   echo "Must supply exactly 1 argument"
   exit 1
 elif [[ $1 == "produce" ]]; then
-  echo "Produce mkdocs tar.gz"
+  echo "Produce tar.gz from $mkdocsProjectPath"
+  if [[ ! -d $mkdocsProjectPath ]]; then
+    echo "mkdocs project path ($mkdocsProjectPath) does not exist, has it been mounted correctly?"
+    exit 1
+  fi
   exit 0
 elif [[ $1 == "serve" ]]; then
   echo "Serve mkdocs"
