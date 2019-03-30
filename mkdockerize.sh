f35e5192fe99df2d4df4454e9147fc25573a08ab
lajmahal <laj.lal@outlook.com>
2019-03-30 21:36:21 +0800

    Refactored the script into smaller functions for readability and structure

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -1,28 +1,49 @@
 #!/usr/bin/env bash
 
-mkdocsProjectPath="/data/mkdocs-project"
-if [[ $# -ne 1 ]]; then
-  echo "Must supply exactly 1 argument"
+function check_num_of_args () {
+  local num_args=$1
+  if [[ num_args -ne 1 ]]; then
+    echo "Must supply exactly 1 argument"
+    exit 1
+  fi
+}
+
+function invalid_command () {
+  echo "Invalid argument - must specify \"produce\" or \"serve\""
   exit 1
-elif [[ $1 == "produce" ]]; then
+}
+
+function produce_mkdocs_tgz () {
+  local mkdocsProjectPath=$1
+
   if [[ ! -d $mkdocsProjectPath ]]; then
     echo "mkdocs project path ($mkdocsProjectPath) does not exist, has it been mounted correctly?"
     exit 1
   fi
+
   cd $mkdocsProjectPath
   mkdocs build > /dev/null
   cp mkdocs.yml site/
   cp -r docs site/
   cd site
   tar -cz *
-elif [[ $1 == "serve" ]]; then
+}
+
+function serve_mkdocs () {
   mkdir site
   cd site
   tar -zxf -
   mv docs mkdocs.yml ../
   cd ../
   mkdocs serve -a 0.0.0.0:8000
+}
+
+# Main script
+check_num_of_args $#
+if [[ $1 == "produce" ]]; then
+  produce_mkdocs_tgz "/data/mkdocs-project"
+elif [[ $1 == "serve" ]]; then
+  serve_mkdocs
 else
-  echo "Invalid argument - must specify \"produce\" or \"serve\""
-  exit 1
+  invalid_command
 fi
