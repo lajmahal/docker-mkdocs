695d12e7c005eac46c1873acb7c87ebb5fb409ed
lajmahal <laj.lal@outlook.com>
2019-03-31 20:39:38 +0800

    Refactor test script to reduce repetitive code

--- test/mkdockerize_test.sh
+++ test/mkdockerize_test.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
 
+function test () {
+  bash ./mkdockerize.sh $@ &> /dev/null
+  retCode=$?
+  if [[ $retCode -eq 1 ]]; then echo "...OK"
+  else
+    echo "expected retCode to be 1 but was $retCode"
+    exit 1;
+  fi
+}
+
 echo "1) Run without arguments should fail"
-bash ./mkdockerize.sh &> /dev/null
-retCode=$?
-if [[ $retCode -eq 1 ]]; then echo "...OK"
-else
-  echo "expected retCode to be 1 but was $retCode"
-  exit 1;
-fi
+test
 
 echo "2) Run with too many arguments should fail"
-bash ./mkdockerize.sh arg1 arg2 &> /dev/null
-retCode=$?
-if [[ $retCode -eq 1 ]]; then echo "...OK"
-else
-  echo "expected retCode to be 1 but was $retCode"
-  exit 1;
-fi
+test arg1 arg2
 
 echo "3) Run with anything other than \"produce\" or \"serve\" should fail"
-bash ./mkdockerize.sh somethingElse &> /dev/null
-retCode=$?
-if [[ $retCode -eq 1 ]]; then echo "...OK"
-else
-  echo "expected retCode to be 1 but was $retCode"
-  exit 1;
-fi
+test somethingElse
